8bbeb8eda48ea63a31cb3f4ff20ec96f1ca76f23
onuragtas <agtasonur@gmail.com>
2019-09-15 22:15:49 +0300

    change install

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ if [ -f ".env" ]; then
 else
   echo "Creating .env"
   cp -n .env.example .env
+
+  echo "Please enter projects path"
+  echo "For example: /project/folder/path"
+
+  read -p "Project Folder:" project_path
+  if [ -z "$project_path" ]
+  then
+        echo "ERROR: Null"
+        exit 1
+  else
+      sed -i "s#./sites_folder#$project_path#g" .env
+      echo "writed..."
+  fi
+
+  echo ""
 fi
-echo ""
-
-echo "Please enter projects path"
-echo "For example: /project/folder/path"
-
-read -p "Project Folder:" project_path
-if [ -z "$project_path" ]
-then
-      echo "ERROR: Null"
-      exit 1
-else
-    sed -i "s#./sites_folder#$project_path#g" .env
-    echo "writed..."
-fi
-
 echo ""
 
 echo "Cleaning up... Configuration for elasticsearch"
