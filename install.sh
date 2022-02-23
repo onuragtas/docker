705edc2fb613d7796b6a215f3005e3cb335d72c7
onuragtas <agtasonur@gmail.com>
2022-02-24 01:22:29 +0300

    test

--- install.sh
+++ install.sh
@@ -97,5 +97,5 @@ echo "Cleaning up... Configuration for elasticsearch"
 sudo sysctl -w vm.max_map_count=262144
 sudo sysctl -w fs.file-max=65536
 cd $WORKDIR
-docker-compose down
+#docker-compose down
 docker-compose up -d --build
