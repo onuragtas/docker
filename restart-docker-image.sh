9536c86c2875982bc5f77e0a579181e0eb9f9640
Onur Ağtaş <agtasonur@gmail.com>
2023-01-24 17:19:36 +0300

    update scripts

--- restart-docker-image.sh
+++ restart-docker-image.sh
@@ -1,6 +1,6 @@
 ips=$(docker container inspect  $(docker ps --format '{{.Names}} {{.Image}}' |grep devenv |awk '{print $1}') --format '{{.Name}}' | sed 's#^/##')
 
-docker pull hakanbysal/devenv:latest
+commands=()
 while IFS= read -r username; do
    password_cmd=$(docker container inspect $username --format='{{index (index (.Config.Env)) 0 }}')
    IFS='='
@@ -8,6 +8,13 @@ while IFS= read -r username; do
    password=${strarr[1]}
 
    port=$(docker container inspect $username --format '{{ (index (index .NetworkSettings.Ports "22/tcp") 0).HostPort }}')
-   echo "docker rm $username -f && bash /docker/serviceip.sh $port $username $password"
-   docker rm $username -f && bash /docker/serviceip.sh $port $username $password
+   commands+=("docker rm $username -f")
+   commands+=("bash /docker/serviceip.sh $port $username $password")
 done <<< "$ips"
+
+docker pull hakanbysal/devenv:latest
+
+for command in "${commands[@]}"
+do
+  eval $command
+done
