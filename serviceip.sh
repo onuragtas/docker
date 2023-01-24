1c662dad9fdb272de06ef94c0cbed8ea3d05970b
Onur Ağtaş <agtasonur@gmail.com>
2023-01-25 01:17:52 +0300

    update scripts

--- serviceip.sh
+++ serviceip.sh
@@ -1,4 +1,17 @@
-docker run --privileged -p $1:22 --name="$2" -d -e "PASSWORD=$3" -v /sites/$2:/sites -v /sites/$2/.nvm:/root/.nvm -v /docker/global/hosts:/etc/hosts --network lemp_net hakanbysal/devenv:latest
+localip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+nginxIp=$(cat global/ip)
+
+add_hosts() {
+  checkHost=$(cat /sites/$1/.hosts/hosts |grep $1.$2.ept-dev.net |awk '{print $1}')
+  if [ "$checkHost" == "" ]; then
+  echo "$localip $1.$2.ept-dev.net" >> /sites/$1/.hosts/hosts
+  fi
+}
+
+mkdir /sites/$2/.hosts -p
+touch /sites/$2/.hosts/hosts
+
+docker run --privileged -p $1:22 --name="$2" -d -e "PASSWORD=$3" -v /sites/$2:/sites -v /sites/$2/.nvm:/root/.nvm -v /sites/$2/.hosts/hosts:/etc/hosts -v /docker/etc/nginx/$2:/usr/local/nginx --network lemp_net hakanbysal/devenv:latest
 
 sleep 5
 
@@ -12,27 +25,15 @@ echo "}" >> /docker/etc/nginx/docker_service_ip.conf
 
 sleep 2
 
-localip=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
-nginxIp=$(cat global/ip)
+docker exec $2 sh -c "echo $2 > /root/.username"
 
-docker exec $2 sh -c "echo $2 > /root/username"
-
-checkHost=$(cat /etc/hosts |grep $2.payment.ept-dev.net |awk '{print $1}')
-if [ "$checkHost" == "" ]; then
-echo "$localip $2.payment.ept-dev.net" >> /etc/hosts
-echo "$localip $2.payment.ept-dev.net" >> /docker/global/hosts
-fi
-
-checkHost=$(cat /etc/hosts |grep $2.epa-api.ept-dev.net |awk '{print $1}')
-if [ "$checkHost" == "" ]; then
-echo "$localip $2.epa-api.ept-dev.net" >> /etc/hosts
-echo "$localip $2.epa-api.ept-dev.net" >> /docker/global/hosts
-fi
-
-checkHost=$(cat /etc/hosts |grep $2.fe.ept-dev.net |awk '{print $1}')
-if [ "$checkHost" == "" ]; then
-echo "$localip $2.epa-api.ept-dev.net" >> /etc/hosts
-echo "$localip $2.epa-api.ept-dev.net" >> /docker/global/hosts
-fi
+add_hosts $2 "epa-api"
+add_hosts $2 "payment"
+add_hosts $2 "fe"
+add_hosts $2 "evo-api"
+add_hosts $2 "order"
+add_hosts $2 "hgs-api"
+add_hosts $2 "admin"
+add_hosts $2 "evo"
 
 docker restart global nginx
