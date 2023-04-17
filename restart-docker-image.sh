8572367ac9f88020cf88dabebdfc1cf81d5556f8
onuragtas <agtasonur@gmail.com>
2023-04-17 20:51:13 +0300

    heapsize changed

--- restart-docker-image.sh
+++ restart-docker-image.sh
@@ -7,7 +7,7 @@ while IFS= read -r username; do
    read -a strarr <<< "$password_cmd"
    password=${strarr[1]}
 
-   port=$(docker container inspect $username --format '{{ (index (index .NetworkSettings.Ports "22/tcp") 0).HostPort }}')
+   port=$(docker container inspect $username --format '{{ (index (index .HostConfig.PortBindings "22/tcp") 0).HostPort }}')
    commands+=("docker rm $username -f")
    commands+=("bash /root/.docker-environment/serviceip.sh $port $username $password")
 done <<< "$ips"
