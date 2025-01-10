d9576bb5a56b65a74bf81a124bfb5c27c20e91c5
onuragtas <agtasonur@gmail.com>
2025-01-11 02:09:55 +0300

    devenv test

--- serviceip.sh
+++ serviceip.sh
@@ -36,7 +36,10 @@ docker run --privileged \
         -v /sites/$2/.configs:/root/.configs \
         -v /root/.docker-environment/etc/nginx/$2:/usr/local/nginx \
         -v /root/.docker-environment/httpd/sites-enabled/$2:/usr/local/httpd \
-        --network lemp_net hakanbaysal/devenv:latest
+        --network lemp_net \
+        --memory="1g" \
+        --memory-swap="1g" \
+        --cpus="1.5" \
 
 sleep 5
 
